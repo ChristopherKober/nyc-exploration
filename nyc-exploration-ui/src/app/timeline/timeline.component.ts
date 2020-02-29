5e2284e7a8c87969aa89294a2131d395c06691d8
Christopher Kober <ChristopherKober@users.noreply.github.com>
2020-02-29 17:42:54 -0500

    Add dynamic timeline and icon integration

--- nyc-exploration-ui/src/app/timeline/timeline.component.ts
+++ nyc-exploration-ui/src/app/timeline/timeline.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { TimelineDatabaseService } from './timeline.service';
+import { DatabaseService } from '../database.service';
+import { MapSelectionService } from '../map-selection.service'
+
 
 @Component({
   selector: 'app-timeline',
@@ -10,19 +12,38 @@ import { TimelineDatabaseService } from './timeline.service';
 export class TimelineComponent implements OnInit {
 
   public timelineData = [{ "EVENT_ID": 1, "EVENT_TYPE": "Meal", "EVENT_NAME": "Da Andrea", "NEIGHBORHOOD_ID": 7, "EVENT_DATE": "1/17/2020", "EVENT_RATING": 4, "COMMENTS": null }, { "EVENT_ID": 2, "EVENT_TYPE": "Meal", "EVENT_NAME": "Jones Wood Foundry", "NEIGHBORHOOD_ID": 36, "EVENT_DATE": "1/18/2020", "EVENT_RATING": 4, "COMMENTS": null }];
-  _DatabaseService;
-  _headline = "Timeline";
 
-  constructor(private http: HttpClient) {
-    this._DatabaseService = new TimelineDatabaseService(http)
+  _DatabaseService;
+  _headline: string;
+  neighborhoodList = [];
+
+  constructor(private http: HttpClient, private mapSelectionService: MapSelectionService) {
+    this._DatabaseService = new DatabaseService(http)
   }
 
   ngOnInit() {
     this.getData()
+    this.mapSelectionService.currentSelection.subscribe(selection => { this.updateTimeline(selection); });
+
   }
 
-  public setNeighborhood(neighborhoodId) {
-    this._headline = "Neighborhood" + neighborhoodId;
+  updateTimeline(neighborhood) {
+    if (neighborhood == null)
+      neighborhood = -1
+
+    var neighborhoodName = "Timeline";
+
+    var neighborhoodElement = this.neighborhoodList.find(n => n["NEIGHBORHOOD_ID"] == neighborhood);
+
+    if (neighborhoodElement == null) {
+      neighborhood = -1;
+    } else {
+      neighborhoodName = neighborhoodElement["DISPLAY_NAME"];
+    }
+
+    this._headline = neighborhoodName;
+    this.getDataForNeighborhood(neighborhood)
+
   }
 
   getData() {
@@ -31,14 +52,49 @@ export class TimelineComponent implements OnInit {
         this.timelineData = data.sort((x, y) => (new Date(x.EVENT_DATE)).getTime() - (new Date(y.EVENT_DATE)).getTime())
           .map(x => {
             x.EVENT_DATE = (new Date(x.EVENT_DATE)).toLocaleDateString()
+
+            var neighborhoodItem = this.neighborhoodList.find(n => n["NEIGHBORHOOD_ID"] == x["NEIGHBORHOOD_ID"]);
+            if (neighborhoodItem != null) {
+              x.DISPLAY_NAME = neighborhoodItem["DISPLAY_NAME"];
+            }
+
             return x;
           })
-
-        console.log(JSON.stringify(data))
-        console.log(JSON.stringify(this.timelineData))
       },
       err => { console.log(err) },
-      () => { console.log('done loading data') }
+      () => { }
+    )
+
+    this._DatabaseService.getNeighborhoods().subscribe(
+      data => {
+        this.neighborhoodList = data;
+      }
+    )
+  }
+
+  getDataForNeighborhood(neighborhood) {
+
+    if (neighborhood == -1) {
+      this.getData();
+      return;
+    }
+
+    this._DatabaseService.getEventsFromNeighborhood(neighborhood).subscribe(
+      data => {
+        this.timelineData = data.sort((x, y) => (new Date(x.EVENT_DATE)).getTime() - (new Date(y.EVENT_DATE)).getTime())
+          .map(x => {
+            x.EVENT_DATE = (new Date(x.EVENT_DATE)).toLocaleDateString()
+            return x;
+          })
+      },
+      err => { console.log(err) },
+      () => { }
+    )
+
+    this._DatabaseService.getNeighborhoods().subscribe(
+      data => {
+        this.neighborhoodList = data;
+      }
     )
   }
 
