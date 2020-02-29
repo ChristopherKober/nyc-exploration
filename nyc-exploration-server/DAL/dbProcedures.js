bc49d13f7f68606c034e9b8635d103302b41b0b1
Christopher Kober <ChristopherKober@users.noreply.github.com>
2020-02-29 17:45:15 -0500

    Update DAL for new endpoints

--- nyc-exploration-server/DAL/dbProcedures.js
+++ nyc-exploration-server/DAL/dbProcedures.js
@@ -1,4 +1,5 @@
 const DAL = require('./DAL')
+const AUTH = require('../api/authentication')
 
 module.exports = {
 
@@ -12,6 +13,44 @@ module.exports = {
 
     getPasswordsFromDatabase: async function () {
         return await DAL.executeQuery('SELECT * FROM public."NYC_CREDENTIALS"')
+    },
+
+    getAllVisitedNeighborhoods: async function () {
+        return await DAL.executeQuery('SELECT DISTINCT "NEIGHBORHOOD_ID" FROM public."NYC_EVENTS" \
+                                       ORDER BY "NEIGHBORHOOD_ID" ASC')
+    },
+
+    getAllEventsFromNeighborhood: async function (neighborhoodId) {
+        if (isNaN(neighborhoodId))
+            return []
+
+        neighborhoodId = Number(neighborhoodId)
+        return await DAL.executeQuery('SELECT * FROM public."NYC_EVENTS" \
+                                       WHERE "NEIGHBORHOOD_ID" = ' + neighborhoodId + ' \
+                                       ORDER BY "EVENT_DATE" DESC')
+    },
+
+    addEvent: async function (params) {
+        try {
+            var auth = await this.getPasswordsFromDatabase();
+            var authed = await AUTH.authenticate(params, auth);
+            console.log(authed)
+            if (authed) {
+
+                return await DAL.executeQuery('INSERT INTO public."NYC_EVENTS"( \
+	                                           "EVENT_TYPE", "EVENT_NAME", "NEIGHBORHOOD_ID", "EVENT_DATE", "EVENT_RATING", "COMMENTS") \
+                                                VALUES(\'' + params.event_type.replace(/[-'"()]/gm,'') + '\'\
+                                                , \'' + params.event_name.replace(/[-'"()]/gm, '') + '\'\
+                                                , ' + params.neighborhood_id.toString().replace(/[-'"()]/gm, '') + '\
+                                                , \'' + params.event_date.replace(/[-'"()]/gm, '') + '\'\
+                                                , ' + params.event_rating.toString().replace(/[-'"()]/gm, '') + '\
+                                                , \'' + params.comments.replace(/[-'"()]/gm, '') + '\'\
+                                                );');
+            }
+        }
+        catch (err) {
+            return;
+        }
     }
 
 }
