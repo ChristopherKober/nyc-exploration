23c86619cdd364fc548e85c7eaf0b98cb7fd333b
Christopher Kober <ChristopherKober@users.noreply.github.com>
2020-02-29 17:45:32 -0500

    Add new endpoints for fetching and saving data

--- nyc-exploration-server/api/api.js
+++ nyc-exploration-server/api/api.js
@@ -11,13 +11,6 @@ module.exports = {
         /// Returns an echo response to test if service is available
         app
             .get('/echo', (req, res) => {
-
-                auth.authenticate(req).then(function (result) {
-                    console.log("Auth returned: " + result.toString())
-                }, function (err) {
-                    console.log("Error occured: " + err.toString())
-                })
-
                 res.statusCode = 200;
                 res.set('Access-Control-Allow-Origin', '*')
                 res.set('Access-Control-Allow-Methods', 'GET')
@@ -49,6 +42,7 @@ module.exports = {
                         res.end('Data format not as expected')                    
                     }
                 }, function (err) {
+                        console.log(err);
                     res.statusCode = 500;
                     res.end('Database Error')
                 })
@@ -57,9 +51,57 @@ module.exports = {
             .options('/events', (req, res) => {
                 res.statusCode = 200
                 res.set('Access-Control-Allow-Origin', '*')
+                res.set('Access-Control-Allow-Methods', 'GET, POST')
+                res.set('Access-Control-Allow-Headers', 'Origin, Content-Type')
+                res.end()
+            })
+
+        app
+            .get('/events/:neighborhood', (req, res) => {
+                dbProcedures.getAllEventsFromNeighborhood(req.params["neighborhood"]).then(function (result) {
+                    try {
+                        res.statusCode = 200;
+                        res.set('Access-Control-Allow-Origin', '*')
+                        res.set('Access-Control-Allow-Methods', 'GET')
+                        res.set('Access-Control-Allow-Headers', 'Origin, Content-Type')
+                        res.end(JSON.stringify(result["rows"]))
+                    } catch {
+                        res.statusCode = 500;
+                        res.end('Data format not as expected')
+                    }
+                }, function (err) {
+                    console.log(err);
+                    res.statusCode = 500;
+                    res.end('Database Error')
+                })
+            })
+        app
+            .options('/events/:neighborhood', (req, res) => {
+                res.statusCode = 200
+                res.set('Access-Control-Allow-Origin', '*')
                 res.set('Access-Control-Allow-Methods', 'GET')
                 res.set('Access-Control-Allow-Headers', 'Origin, Content-Type')
                 res.end()
+            })
+
+        app
+            .post('/events', (req, res) => {
+                dbProcedures.addEvent(req.body).then(function (result) {
+                    try {
+                        res.statusCode = 200;
+                        res.set('Access-Control-Allow-Origin', '*')
+                        res.set('Access-Control-Allow-Methods', 'GET')
+                        res.set('Access-Control-Allow-Headers', 'Origin, Content-Type')
+                        res.end(JSON.stringify(result["rows"]))
+                    } catch {
+                        res.statusCode = 500;
+                        res.end('Data format not as expected')
+                    }
+                }, function (err) {
+                    console.log(err);
+                    res.statusCode = 500;
+                    res.end('Database Error')
+                })
             })
 
         app
@@ -83,6 +125,34 @@ module.exports = {
 
         app
             .options('/neighborhoods', (req, res) => {
+                res.statusCode = 200
+                res.set('Access-Control-Allow-Origin', '*')
+                res.set('Access-Control-Allow-Methods', 'GET')
+                res.set('Access-Control-Allow-Headers', 'Origin, Content-Type')
+                res.end()
+            })
+
+        app
+            .get('/neighborhoods/visited', (req, res) => {
+                dbProcedures.getAllVisitedNeighborhoods().then(function (result) {
+                    try {
+                        res.statusCode = 200;
+                        res.set('Access-Control-Allow-Origin', '*')
+                        res.set('Access-Control-Allow-Methods', 'GET')
+                        res.set('Access-Control-Allow-Headers', 'Origin, Content-Type')
+                        res.end(JSON.stringify(result["rows"]))
+                    } catch {
+                        res.statusCode = 500;
+                        res.end('Data format not as expected')
+                    }
+                }, function (err) {
+                    res.statusCode = 500;
+                    res.end('Database Error')
+                })
+            })
+
+        app
+            .options('/neighborhoods/visited', (req, res) => {
                 res.statusCode = 200
                 res.set('Access-Control-Allow-Origin', '*')
                 res.set('Access-Control-Allow-Methods', 'GET')
--- nyc-exploration-server/index.js
+++ nyc-exploration-server/index.js
@@ -1,10 +1,21 @@
 const express = require('express')
+var timeout = require('connect-timeout')
+
 const PORT = process.env.PORT || 5000
 const api = require('./api/api')
 
 const app = express()
 
 
+
+app.use(timeout(15000));
+app.use(haltOnTimedout);
+
+function haltOnTimedout(req, res, next) {
+    if (!req.timedout) next();
+}
+
+app.use(express.json())
 api.setup(app)
 
 app
